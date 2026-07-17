b9cedb7e3f2b17447d63ac5de64ae6f7dc62fba8
EfeEriomala <167799890+EfeEriomala@users.noreply.github.com>
2026-07-17 09:39:07 +0100

    Delete Chamber Of Commers directory
    yes

--- Chamber Of Commers/scripts/date.js
+++
@@ -1,5 +0,0 @@
-//lastmodified
-function displayCurrentYear() { document.getElementById("currentyear").innerHTML = new Date().getFullYear(); };
-displayCurrentYear();
-
-document.getElementById("lastModified").innerHTML = "Last Updated " + document.lastModified;
